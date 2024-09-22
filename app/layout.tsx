a91142bf8b3a9760e03ce9f522a06f2b5df79cc9
Raj-Hinkal <raj@hinkal.pro>
2024-09-22 02:54:08 -0400

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+export const dynamic = 'force-dynamic'
+
 import type { Metadata } from "next";
-import { Inter, IBM_Plex_Serif } from 'next/font/google';
+import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter'});
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ['latin'],
   weight: ['400', '700'],
@@ -13,7 +15,7 @@ export const metadata: Metadata = {
   title: "Pibbi Bank",
   description: "Pibbi Bank is a modern banking platform for everyone.",
   icons: {
-    icon: 'icon/logo.svg'
+    icon: '/icons/logo.svg'
   }
 };
 
@@ -24,11 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} ${ibmPlexSerif.variable}`}
-      >
-        {children}
-      </body>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
     </html>
   );
 }
